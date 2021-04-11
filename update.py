f1f0b7f8cac591b3e59b984f2747923790147a77
danieljstone <66531951+danieljstone@users.noreply.github.com>
2021-04-11 13:18:47 -0400

    update

--- update.py
+++ update.py
@@ -34,17 +34,20 @@ def deathupdate():
             req = requests.get(url)
                 # this fixes an annoying problem where some dates without reports redirect to pages for other dates
             if req.url == url:
-                soup=BeautifulSoup(req.text, "lxml")    
-                date=soup.select("div.content.clearfix > div.field.field-name-field-date.field-type-date.field-label-hidden > div > div > span").__str__().split('\"')[3][:10]
-                b=soup.find_all(string=re.compile("year-old"))
-                for x in b:
-                    newline={}
-                    newline["date"]=date
-                    y=x.__str__().split()
-                    newline["age"]=y[0].split("-")[0]
-                    newline["gender"]=y[1]
-                    newline["url"]=url
-                    deaths=deaths.append(newline, ignore_index=True)
+                try:
+                    soup=BeautifulSoup(req.text, "lxml")    
+                    date=soup.select("div.content.clearfix > div.field.field-name-field-date.field-type-date.field-label-hidden > div > div > span").__str__().split('\"')[3][:10]
+                    b=soup.find_all(string=re.compile("year-old"))
+                    for x in b:
+                        newline={}
+                        newline["date"]=date
+                        y=x.__str__().split()
+                        newline["age"]=y[0].split("-")[0]
+                        newline["gender"]=y[1]
+                        newline["url"]=url
+                        deaths=deaths.append(newline, ignore_index=True)
+                except:
+                    pass
             else:
                 pass
             time.sleep(3)
@@ -61,6 +64,8 @@ def deathupdate():
        
             
 deathupdate()
+
+
 
 
 
